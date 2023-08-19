8a40b49b8edb7ba4f0a6d4c737b8d2e2f376c8c4
Sabina Kromraj <sabina.kromraj@gmail.com>
2023-08-19 20:57:55 +0200

    loop for main.py

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ shopping_dict = {
     'warzywniak': ['marchew', 'seler', 'rukola']
 }
 capi_shopping_dict = {key.capitalize(): [value.capitalize() for value in values] for key, values in shopping_dict.items()}
+
+value_count = 0
+
+for key, value in capi_shopping_dict.items():
+    print(f"Idę do {key}, kupuję tu następujące rzeczy: {value}")
+    value_count += len(value)
+    
+print(f"W sumie kupuję {value_count} produktów.")
