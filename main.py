a26f3973dd5bcce174690a6c214e5a20657dc999
Sabina Kromraj <sabina.kromraj@gmail.com>
2023-08-19 20:54:50 +0200

    capitalize main.py

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ shopping_dict = {
     'piekarnia': ['chleb', 'pączek', 'bułki'],
     'warzywniak': ['marchew', 'seler', 'rukola']
 }
+capi_shopping_dict = {key.capitalize(): [value.capitalize() for value in values] for key, values in shopping_dict.items()}
